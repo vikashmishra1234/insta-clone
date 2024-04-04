66736d1c8c1d0efd06326349119d792415bd459e
Vikash Mishra <131462517+vikashmishra1234@users.noreply.github.com>
2024-04-04 21:58:38 +0530

    Update index.js

--- insta-backend/index.js
+++ insta-backend/index.js
@@ -8,7 +8,11 @@ const app = express();
 
 const Port = 2000;
 
-app.use(cors());
+app.use(cors({
+    origin:["https://insta-clone-frontend-sooty.vercel.app"],
+    methods:["POST","GET","PUT"],
+    credentials:true
+}));
 app.use(express.json())
 app.use('/api',routes);
 app.use(cookieParser())
