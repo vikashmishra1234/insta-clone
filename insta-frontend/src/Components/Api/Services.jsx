0c14cddd1a64b07e66704f5e150b89e5d3ee6aa3
vikash mishra <vikashmishra8371@gmail.com>
2024-04-05 00:21:16 +0530

    ls

--- insta-frontend/src/Components/Api/Services.jsx
+++ insta-frontend/src/Components/Api/Services.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 // const url = 'http://localhost:2000';
-const url = 'https://insta-clone-2tsp.onrender.com';
+const url = 'https://insta-clone-knbn.onrender.com';
 
 
 
